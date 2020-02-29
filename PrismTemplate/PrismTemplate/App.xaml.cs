569c3e28c1aef777b1c54a17d1ae43319b9e0acb
Tuyen Le <tuyenktdn93@gmail.com>
2020-02-29 22:42:06 +0700

    add appcenter analytics and crash

--- PrismTemplate/PrismTemplate/App.xaml.cs
+++ PrismTemplate/PrismTemplate/App.xaml.cs
@@ -6,6 +6,9 @@ using PrismTemplate.ViewModels;
 using PrismTemplate.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace PrismTemplate
@@ -19,6 +22,9 @@ namespace PrismTemplate
         protected override async void OnInitialized()
         {
             InitializeComponent();
+            AppCenter.Start("ios=d7b45b5d-0498-449d-a415-86ce3eb10780;" +
+                  "android=646efe20-b1f5-4369-bd06-3eb9743b19e0",
+                  typeof(Analytics), typeof(Crashes));
             await NavigationService.NavigateAsync("NavigationPage/MainPage");
         }
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
