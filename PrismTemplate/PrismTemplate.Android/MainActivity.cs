98fddc4a53a54daa5ca8243eb87d7e84099567b4
Tuyen Le <tuyenlt@mti-tech.vn>
2020-04-08 18:24:46 +0700

    setup notification hubs

--- PrismTemplate/PrismTemplate.Android/MainActivity.cs
+++ PrismTemplate/PrismTemplate.Android/MainActivity.cs
@@ -7,17 +7,38 @@ using Prism;
 using Prism.Ioc;
 using StatesButton.Android.Renderers;
 using Microsoft.AppCenter.Distribute;
+using Android.Util;
+using Android.Gms.Common;
 
 namespace PrismTemplate.Droid
 {
     [Activity(Label = "PrismTemplate", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public const string TAG = "MainActivity";
+        internal static readonly string CHANNEL_ID = "tuyen_notification_channel";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
             base.OnCreate(savedInstanceState);
+
+            if (Intent.Extras != null)
+            {
+                foreach (var key in Intent.Extras.KeySet())
+                {
+                    if (key != null)
+                    {
+                        var value = Intent.Extras.GetString(key);
+                        Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
+                    }
+                }
+            }
+
+            IsPlayServicesAvailable();
+            CreateNotificationChannel();
+
             UserDialogs.Init(this);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
@@ -41,6 +62,46 @@ namespace PrismTemplate.Droid
             {
                 // Register any platform specific implementations
             }
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    Log.Debug(TAG, GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                else
+                {
+                    Log.Debug(TAG, "This device is not supported");
+                    Finish();
+                }
+                return false;
+            }
+
+            Log.Debug(TAG, "Google Play Services is available.");
+            return true;
+        }
+
+        private void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channelName = CHANNEL_ID;
+            var channelDescription = string.Empty;
+            var channel = new NotificationChannel(CHANNEL_ID, channelName, NotificationImportance.Default)
+            {
+                Description = channelDescription
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
         }
     }
 }
---
+++ PrismTemplate/PrismTemplate/Constants/NotificationHubsConstants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace PrismTemplate.Constants
+{
+    public static class NotificationHubsConstants
+    {
+        public const string ListenConnectionString = "Endpoint=sb://tuyennotihubsdemo1.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=hk/qToXdQvGewsJNCFumvwcunzZiUTBNk6Oznzjhbrw=";
+        public const string NotificationHubName = "sendnotihubsdemo1";
+    }
+}
